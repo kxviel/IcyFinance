53f7af85e3d99ca7b2cb34460bcb2d229f2ca563
kxviel <kevin.dgoveas@gmail.com>
2026-09-20 20:54:47 +0200

    feat: oxc

--- vite.config.ts
+++ vite.config.ts
@@ -6,24 +6,34 @@ import { defineConfig } from "vite";
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
-	plugins: [
-		tanstackRouter({ target: "react", autoCodeSplitting: true }),
-		react(),
-		tailwindcss(),
-	],
-	resolve: {
-		tsconfigPaths: true,
-	},
-	clearScreen: false,
-	server: {
-		port: 1420,
-		strictPort: true,
-		host: host || "127.0.0.1",
-		hmr: host ? { protocol: "ws", host, port: 1421 } : undefined,
-		watch: { ignored: ["**/src-tauri/**"] },
-	},
-	build: {
-		target: ["es2022", "chrome105", "safari15.4"],
-		sourcemap: false,
-	},
+  /* React */
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
+    react({
+      compiler: true,
+    }),
+  ],
+
+  /* Aliasing */
+  resolve: {
+    tsconfigPaths: true,
+  },
+
+  /* Tauri */
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || "127.0.0.1",
+    hmr: host ? { protocol: "ws", host, port: 1421 } : undefined,
+    watch: { ignored: ["**/src-tauri/**"] },
+  },
+  build: {
+    target: ["es2022", "chrome105", "safari15.4"],
+    sourcemap: false,
+  },
 });
